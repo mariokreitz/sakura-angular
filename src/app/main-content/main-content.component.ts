5a9bc58ba397ad6812459bad11f5e9ba1aefebf0
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-23 10:33:56 +0100

    Update main-content.component.ts

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { OurMenuComponent } from './our-menu/our-menu.component';
+import { HowToOrderComponent } from './how-to-order/how-to-order.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [LandingPageComponent],
+  imports: [LandingPageComponent, OurMenuComponent, HowToOrderComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
 })
