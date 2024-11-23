3051129019d9bbcc634c708e5d986a6e2741d7cc
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-23 08:38:56 +0100

    Update landingPage.component.ts

--- src/app/landingPage/landingPage.component.ts
+++ src/app/landingPage/landingPage.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-landingPage',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   template: `
     <section>
+      <app-navbar />
       <h1 class="fontRaleway">SAKURA RAMEN</h1>
       <h2 class="fontRaleway">BEST RAMEN IN TOWN</h2>
     </section>
