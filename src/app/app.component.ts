d237d431385afa3ca0a92d581367edd249570149
Mario Kreitz <Mario.Kreitz@web.de>
2024-11-23 10:33:47 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainContentComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MainContentComponent,
+    FooterComponent,
+    ImprintComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
